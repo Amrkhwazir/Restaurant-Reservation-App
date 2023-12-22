455c01e3001179d2dab35c5d41083b836283fbec
Amrkhwazir <amrkh5678@gmail.com>
2023-12-22 22:09:43 +0500

    ticket booking app completed

--- src/pages/Hotel.jsx
+++ src/pages/Hotel.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from './List'
 import Header from '../component/Header'
 import Navbar from '../component/Navbar'
 import styled from 'styled-components'
-import {  faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import {  faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import MailList from '../component/MailList'
 import Footer from '../component/Footer'
@@ -24,6 +24,31 @@ display: flex;
 flex-direction: column;
 align-items: center;
 `;
+
+const HotelSlider = styled.div`
+position: sticky;
+top: 0;
+left: 0;
+width: 100vw;
+height: 100vh;
+background-color: rgba(0, 0, 0, 0.426);
+display: flex;
+align-items: center;
+`;
+
+const SliderWrapper = styled.div`
+width:100%;
+height:100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+const SlideImg = styled.img`
+  width: 80%;
+  height: 80vh;
+  
+`;
+
 const HotelDetail = styled.div`
   width: 100%;
   max-width: 1024px;
@@ -124,12 +149,71 @@ const Detailprice = styled.p`
 `;
 
 const Hotel = () => {
+
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+  const photos  = [
+    {
+      src:'https://th.bing.com/th/id/R.d1ad93015e8f92033dc884aa6c12c480?rik=PhQDSTGRxb5ZUw&pid=ImgRaw&r=0'
+    },
+    {
+      src:'https://th.bing.com/th/id/R.b1e1dfafa938387724d864593512d84f?rik=EwfUQD7xDNucEw&pid=ImgRaw&r=0'
+    },
+    {
+      src:'https://th.bing.com/th/id/OIP._UFYXPXPeQjs4Oy4Pq38VAHaFj?rs=1&pid=ImgDetMain'
+    },
+    {
+      src:'https://i2.wp.com/www.remodelingcosts.org/wp-content/uploads/2016/11/spacious-bathroom.jpg'
+    },
+    {
+      src:'https://th.bing.com/th/id/R.33a020e8e5ac0ef66b6dc6e24635642d?rik=eKBzUtIQcD67TA&pid=ImgRaw&r=0&sres=1&sresct=1'
+    },
+    {
+      src:'https://th.bing.com/th/id/OIP.u_051WJmnzt3mOoaBbATmgHaEr?rs=1&pid=ImgDetMain'
+    },
+    {
+      src:'https://th.bing.com/th/id/R.02098baa170857c5ecf4b4db033841cd?rik=cOb1FOge48HdXQ&riu=http%3a%2f%2fcdn.home-designing.com%2fwp-content%2fuploads%2f2018%2f07%2fstudy-room-ideas.jpg&ehk=PMlclYVIgjwOsKCG6vpnLJtS7FbS0ckx3RPHm%2bfj0a8%3d&risl=&pid=ImgRaw&r=0'
+    },
+    {
+      src:'https://th.bing.com/th/id/R.11b31697093428871cfefd3a9c443700?rik=rqizN11wSdmR7Q&riu=http%3a%2f%2fwww.stevewilliamskitchens.co.uk%2fwp-content%2fuploads%2f2016%2f04%2fexamples-of-balcony-modern-interior-design-10.jpg&ehk=qT%2ftIVMs6sWLP1JgtpgW%2bHRDBP5FhT5sFioAXhlW3Lo%3d&risl=&pid=ImgRaw&r=0'
+    },
+    {
+      src:'https://th.bing.com/th/id/R.0bd832ffca75f32a132ca5f4451ad62d?rik=Djm%2bpC5hJ00yGg&pid=ImgRaw&r=0'
+    },
+  ];
+
+  const openHandler = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+  }
+
+
+  const handleMove = (direction) =>{
+    let newSliderNumber;
+    if(direction === "l") {
+      newSliderNumber = slideNumber === 0 ? 5 : slideNumber - 1
+    }else{
+      newSliderNumber = slideNumber === 5 ? 0 : slideNumber + 1
+
+    }
+
+    setSlideNumber(newSliderNumber);
+  }
   return (
     <div>
       <Navbar />
       <Header type="list"/>
       <HomeContainer>
       <HotelContainer>
+      {open &&
+        <HotelSlider>
+        <FontAwesomeIcon icon={faCircleXmark} style={{position: "absolute", top: "20px", right: "20px", fontSize: "30px", color: "lightgrey", cursor: "pointer"}}  onClick={()=> setOpen(false)}/>
+        <FontAwesomeIcon icon={faCircleArrowLeft}  style={{margin: "20px", fontSize: "50px", color: "lightgray", cursor: "pointer"}} onClick={()=> handleMove("l")}/>
+        <SliderWrapper>
+        <SlideImg src={photos[slideNumber].src} />
+        </SliderWrapper>
+        <FontAwesomeIcon icon={faCircleArrowRight}style={{margin: "20px", fontSize: "50px", color: "lightgray", cursor: "pointer"}}  onClick={()=> handleMove("r")}/>
+        </HotelSlider>}
         <HotelDetail>
           <HotelNavigation>
             <HotelTitle>Tower Street Apartments</HotelTitle>
@@ -142,15 +226,12 @@ const Hotel = () => {
           </BookNow>
         </HotelDetail>
         <HotelImages>
-          <HotelImg src='https://th.bing.com/th/id/R.d1ad93015e8f92033dc884aa6c12c480?rik=PhQDSTGRxb5ZUw&pid=ImgRaw&r=0'/>
-          <HotelImg src='https://th.bing.com/th/id/R.b1e1dfafa938387724d864593512d84f?rik=EwfUQD7xDNucEw&pid=ImgRaw&r=0'/>
-          <HotelImg src='https://th.bing.com/th/id/OIP._UFYXPXPeQjs4Oy4Pq38VAHaFj?rs=1&pid=ImgDetMain'/>
-          <HotelImg src='https://i2.wp.com/www.remodelingcosts.org/wp-content/uploads/2016/11/spacious-bathroom.jpg'/>
-          <HotelImg src='https://th.bing.com/th/id/R.33a020e8e5ac0ef66b6dc6e24635642d?rik=eKBzUtIQcD67TA&pid=ImgRaw&r=0&sres=1&sresct=1'/>
-          <HotelImg src='https://th.bing.com/th/id/OIP.u_051WJmnzt3mOoaBbATmgHaEr?rs=1&pid=ImgDetMain'/>
-          <HotelImg src='https://th.bing.com/th/id/R.02098baa170857c5ecf4b4db033841cd?rik=cOb1FOge48HdXQ&riu=http%3a%2f%2fcdn.home-designing.com%2fwp-content%2fuploads%2f2018%2f07%2fstudy-room-ideas.jpg&ehk=PMlclYVIgjwOsKCG6vpnLJtS7FbS0ckx3RPHm%2bfj0a8%3d&risl=&pid=ImgRaw&r=0'/>
-          <HotelImg src='https://th.bing.com/th/id/R.11b31697093428871cfefd3a9c443700?rik=rqizN11wSdmR7Q&riu=http%3a%2f%2fwww.stevewilliamskitchens.co.uk%2fwp-content%2fuploads%2f2016%2f04%2fexamples-of-balcony-modern-interior-design-10.jpg&ehk=qT%2ftIVMs6sWLP1JgtpgW%2bHRDBP5FhT5sFioAXhlW3Lo%3d&risl=&pid=ImgRaw&r=0'/>
-          <HotelImg src='https://th.bing.com/th/id/R.0bd832ffca75f32a132ca5f4451ad62d?rik=Djm%2bpC5hJ00yGg&pid=ImgRaw&r=0'/>
+          {
+            photos.map((photo, i)=>{
+              return   <HotelImg onClick={() => openHandler(i)} src={photo.src} alt='there is image'/>
+            })
+          }
+        
         </HotelImages>
         <HotelDesc>
           <HotelSpecification>
